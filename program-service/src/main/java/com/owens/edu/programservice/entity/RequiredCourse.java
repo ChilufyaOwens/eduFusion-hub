162a7b1fa82c5160dc548c3d7226edd34f37c27f
Chilufya Owens <74659928+ChilufyaOwens@users.noreply.github.com>
2023-08-27 23:27:01 +0200

    Added another entity

---
+++ program-service/src/main/java/com/owens/edu/programservice/entity/RequiredCourse.java
@@ -0,0 +1,29 @@
+package com.owens.edu.programservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "tbl_required_course")
+public class RequiredCourse extends ProgramJpaAudit<Long> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, optional = false)
+    @JoinColumn(name = "admission_requirements_id", referencedColumnName = "id")
+    @ToString.Exclude
+    private AdmissionRequirements admissionRequirements;
+
+    private String name;
+    private String description;
+    private  Integer minimumCredits;
+
+}
