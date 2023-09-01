a4c86087f35072a8949d5ea0604178d05149ad6d
Chilufya Owens <74659928+ChilufyaOwens@users.noreply.github.com>
2023-09-01 23:32:31 +0200

    #14 - Implemented curriculum service

--- program-service/src/main/java/com/owens/edu/programservice/repository/CurriculumRepository.java
+++ program-service/src/main/java/com/owens/edu/programservice/repository/CurriculumRepository.java
@@ -1,7 +1,12 @@
 package com.owens.edu.programservice.repository;
 
 import com.owens.edu.programservice.entity.Curriculum;
+import com.owens.edu.programservice.entity.Program;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CurriculumRepository extends JpaRepository<Curriculum, Long> {
+
+    Optional<Curriculum> findCurriculumByProgram(Program program);
 }
